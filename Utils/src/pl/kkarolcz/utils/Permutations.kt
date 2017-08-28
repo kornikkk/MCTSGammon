2d00335eaeb9ca445c2912c9b2655d11d61d4334
kornikkk <kornik1992@gmail.com>
2017-08-28 12:49:28 +0200

    * Added Permutations iterator needed for dices

---
+++ Utils/src/pl/kkarolcz/utils/Permutations.kt
@@ -0,0 +1,70 @@
+package pl.kkarolcz.utils
+
+import java.util.*
+
+
+/**
+ * Created by kkarolcz on 28.08.2017.
+ */
+
+inline fun <reified T> permutations(elements: Iterable<T>): Sequence<Array<T>> =
+        PermutationIterator(elements.toList().toTypedArray().copyOf()).asSequence()
+
+/**
+ * Permutations as Iterator implementation of the QuickPerm algorithm described by Phillip Paul Fuchs at http://www.quickperm.org
+ */
+@PublishedApi internal
+class PermutationIterator<T>(private val elements: Array<T>) : Iterator<Array<T>> {
+    private val n = elements.size
+    private val p = IntArray(n)
+    private var i = 1
+
+    private var permutationsCount = n.factorial()
+    private var currentPermutation = 0L
+
+    override fun hasNext(): Boolean {
+        return currentPermutation == permutationsCount
+    }
+
+    override fun next(): Array<T> {
+        if (currentPermutation == 0L) {
+            ++currentPermutation
+            return elements
+        }
+        while (i < n) {
+            if (p[i] < i) {
+                val j = if (i % 2 == 0) 0 else p[i]
+                swap(elements, i, j)
+
+                p[i]++
+                i = 1
+
+                ++currentPermutation
+                return elements
+            } else {
+                p[i] = 0
+                ++i
+            }
+        }
+        throw NoSuchElementException()
+    }
+
+    private fun <T> swap(elements: Array<T>, i: Int, j: Int) {
+        val temp = elements[i]
+        elements[i] = elements[j]
+        elements[j] = temp
+    }
+
+    private fun Int.factorial(): Long {
+        var number = this
+        if (number == 0)
+            return 1
+
+        var result = 1L
+        while (number > 0) {
+            result *= number
+            number--
+        }
+        return result
+    }
+}
---
+++ Utils/test/pl/kkarolcz/utils/PermutationsTest.kt
@@ -0,0 +1,35 @@
+package pl.kkarolcz.utils
+
+import org.junit.Test
+import java.util.*
+import kotlin.test.assertTrue
+
+/**
+ * Created by kkarolcz on 28.08.2017.
+ */
+class PermutationsTest {
+    @Test
+    fun `Test permutations`() {
+        val expected = mutableListOf<Array<Int>>()
+        expected.add(createExpectedArray(1, 2, 3))
+        expected.add(createExpectedArray(1, 3, 2))
+        expected.add(createExpectedArray(2, 1, 3))
+        expected.add(createExpectedArray(2, 3, 1))
+        expected.add(createExpectedArray(3, 1, 2))
+        expected.add(createExpectedArray(3, 2, 1))
+
+        val range = IntRange(1, 3)
+        val permutations = permutations(range)
+        permutations.forEach { permutation -> assertTrue(containsPermutation(expected, permutation)) }
+    }
+
+    private fun containsPermutation(expected: Iterable<Array<Int>>, actual: Array<Int>): Boolean {
+        return expected.any { permutation -> Arrays.equals(permutation, actual) }
+    }
+
+    private fun createExpectedArray(vararg elements: Int): Array<Int> {
+        val array: Array<Int> = Array(elements.size) { 0 }
+        (0 until elements.size).forEach { i -> array[i] = elements[i] }
+        return array
+    }
+}
